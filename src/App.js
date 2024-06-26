b0d56ebf1e9e14190078443121f1f487606fba76
mohseni <nimabk@amazon.com>
2024-06-25 22:26:40 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>CS 230L</h1>
+        <h2>Section - 02</h2>
+        <p>WVU ID: 800323659</p>
+        <p>Hi I am Banafsheh Adami</p>
+      </div>
   );
 }
 
